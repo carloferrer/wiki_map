0845033ea5706473a7060802b5722bdf1af87698
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-28 23:24:40 +0000

    about me rollback function

--- db/migrations/20170928214421_aboutME.js
+++ db/migrations/20170928214421_aboutME.js
@@ -6,5 +6,7 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.table('users').dropColumn('about_me');
+  return knex.schema.table('users', function(table) {
+    table.dropColumn('about_me');
+  });
 };
