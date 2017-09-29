08f12740a4f4148d1b6954f25cab3757d580d254
vagrant <thecarloferrer@gmail.com>
2017-09-29 21:15:52 +0000

    Typing into search box and submitting query populates map w/ relevant things.

--- public/scripts/maps_id.js
+++ public/scripts/maps_id.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   }
 
   function reloadDetails(map) {
-    $('#map-title').text('Welcome to: '+map.title);
+    $('#map-title').text('Map: '+map.title);
   }
 
   function initMap() {
@@ -29,6 +29,7 @@ $(document).ready(function() {
     });
 
     geolocate(map, navigator.geolocation);
+    searchPlace(map);
   }
 
   function geolocate(map, geolocation) {
@@ -54,78 +55,84 @@ $(document).ready(function() {
     }
   }
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                            'Error: The Geolocation service failed.' :
-                            'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  function searchPlace(map) {
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        var newMark = new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(newMark,'click',function(){
+          console.log(place.geometry.location.lat()+' '+place.geometry.location.lng());
+        });
+
+        markers.push(newMark);
+
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+
+      map.fitBounds(bounds);
+    });
+
+  }
 
 });
 
-// // Create the search box and link it to the UI element.
-//   var input = document.getElementById('pac-input');
-//   var searchBox = new google.maps.places.SearchBox(input);
 
-//   // Bias the SearchBox results towards current map's viewport.
-//   map.addListener('bounds_changed', function() {
-//     searchBox.setBounds(map.getBounds());
-//   });
-
-//   var markers = [];
-//   // Listen for the event fired when the user selects a prediction and retrieve
-//   // more details for that place.
-//   searchBox.addListener('places_changed', function() {
-//     var places = searchBox.getPlaces();
-
-//     if (places.length == 0) {
-//       return;
-//     }
-
-//     // Clear out the old markers.
-//     markers.forEach(function(marker) {
-//       marker.setMap(null);
-//     });
-//     markers = [];
-
-//     // For each place, get the icon, name and location.
-//     var bounds = new google.maps.LatLngBounds();
-//     places.forEach(function(place) {
-//       if (!place.geometry) {
-//         console.log("Returned place contains no geometry");
-//         return;
-//       }
-//       var icon = {
-//         url: place.icon,
-//         size: new google.maps.Size(71, 71),
-//         origin: new google.maps.Point(0, 0),
-//         anchor: new google.maps.Point(17, 34),
-//         scaledSize: new google.maps.Size(25, 25)
-//       };
-
-//       // Create a marker for each place.
-//       var newMark = new google.maps.Marker({
-//         map: map,
-//         icon: icon,
-//         title: place.name,
-//         position: place.geometry.location
-//       });
-
-//       google.maps.event.addListener(newMark,'click',function(){
-//         console.log(place.geometry.location.lat()+' '+place.geometry.location.lng());
-//       });
-
-//       markers.push(newMark);
-
-
-//       if (place.geometry.viewport) {
-//         // Only geocodes have viewport.
-//         bounds.union(place.geometry.viewport);
-//       } else {
-//         bounds.extend(place.geometry.location);
-//       }
-//     });
-
-//     map.fitBounds(bounds);
-//   });
