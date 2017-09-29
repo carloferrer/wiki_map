8099b33e11abd0e3e03c4a65580869f357e2c279
vagrant <thecarloferrer@gmail.com>
2017-09-29 18:40:21 +0000

    Started implementation of single map viewing.

---
+++ public/scripts/maps_id.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+
+//   loadMapIndex();
+
+//   function loadMapIndex(){
+//     $.get('http://localhost:8080/api/maps')
+//     .done(function(maps) {
+//       for (let i = 0; i < maps.length; i++) {
+//         $('#map-list').append('<li>'+maps[i].title+'<br>');
+//       }
+
+//     })
+//     .fail(function(error) {
+//       console.error(error);
+//     });
+//   }
+
+// });
