aae2823942ab1ab75ee7080cb63bc26caf3fdce3
vagrant <thecarloferrer@gmail.com>
2017-09-29 18:16:58 +0000

    localhost:8080/maps shows a page with the entire list of map titles.

--- public/scripts/map_list.js
+++ public/scripts/map_list.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
 
   function loadMapIndex(){
     $.get('http://localhost:8080/api/maps')
-    .done(function(result) {
-      console.log(result);
+    .done(function(maps) {
+      for (let i = 0; i < maps.length; i++) {
+        $('#map-list').append('<li>'+maps[i].title+'<br>');
+      }
+
     })
     .fail(function(error) {
       console.error(error);
