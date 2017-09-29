e7f3fc8dde30f3207fe3ef86942a1a159fb93aca
vagrant <thecarloferrer@gmail.com>
2017-09-29 21:06:47 +0000

    Refactored API geolocation function.

--- public/scripts/maps_id.js
+++ public/scripts/maps_id.js
@@ -22,35 +22,37 @@ $(document).ready(function() {
   }
 
   function initMap() {
-    var map, infoWindow;
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -33.8688, lng: 151.2195},
       zoom: 13,
       mapTypeId: 'roadmap'
     });
 
-    infoWindow = new google.maps.InfoWindow;
+    geolocate(map, navigator.geolocation);
+  }
 
-    // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
+  function geolocate(map, geolocation) {
+    var infoWindow = new google.maps.InfoWindow;
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          infoWindow.open(map);
-          map.setCenter(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
+    if (geolocation) {
+      geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
     }
+  }
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       infoWindow.setPosition(pos);
