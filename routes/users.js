a33eac34918f07836b2ed6d559e1cb0136bb0c3f
Ali Abji <aliabji@gmail.com>
2017-09-30 12:53:35 +0000

    basic profile with username and about me section adding from database

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,18 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test")
     knex
     .select("*")
     .from("users")
-    .where("id", "=", req.params.id)
+    .where("id", req.params.id)
+    .then((results) => {
+      console.log(results)
+      console.log("test")
+      res.json(results)
+    })
   })
 
   return router;
 }
+
