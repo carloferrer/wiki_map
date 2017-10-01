8384b21eaa227139df9f25a77b0ac670c2e99324
vagrant <thecarloferrer@gmail.com>
2017-10-01 00:49:15 +0000

    Fixed typos in faker-seeds.js.

--- server.js
+++ server.js
@@ -163,10 +163,7 @@ app.post("/logout", (req, res) => {
 
 //user profile page
 app.get("/users/:id", (req, res) => {
-<<<<<<< HEAD
-=======
 
->>>>>>> 4d6918f175ab131cc8ca84a9d4f79f8d5bff25be
   let userVerification = {user_id: req.session.id}
   if (req.session.id) {
     return res.render("profile", userVerification)
