d3272a48b1708be78ec4d33f57909d6b1ed49ee7
Ali Abji <aliabji@gmail.com>
2017-10-02 13:21:06 +0000

    profile page no longer printing duplicate maps in contributed list

--- public/scripts/active-button.js
+++ public/scripts/active-button.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-  $('a').on('click',function(){
-    $('a').removeClass('selected');
-    $(this).addClass('selected');
-  });
+  $('.home').addClass('.selected')
 })
